0f640cfd4265e9c3041f38ab3d38540a118ee0da
user <d_k81@mail.ru>
2025-07-26 20:42:48 +0500

    коммит из фьютер добавил часть кода по уроку

---
+++ src/reports.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def report_to_file(..):
+    def inner(..):
+        @wraps(..)
+        def wrapper(..):
+
+
+
+
+@report_to_file('fast_food_report.json')
+def spending_by_category(transactions: pd.DataFrame,
+                         category: str,
+                         date: Optional[str] = None) -> pd.DataFrame:
+    date_dt =
+    start_dt = date_dt - pd.DateOffset(months=3)
+    pass
